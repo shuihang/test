2965741ba54f9c7c91f2d5f8d6f0c4688f912ce0
shuihang <2680027613@qq.com>
2024-01-07 22:49:22 +0800

    fix: 修改config.default.js配置

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@
 // const mongodbUrl = 'mongodb://root:admin123@localhost:27017/cjx-cli'
 const user = 'root'
 const password = 'cjx6Ufg8UFoQRtBMx'
-// const mongodbUrl = 'mongodb://root:cjx6Ufg8UFoQRtBMx@39.100.76.235:27017/cjx-cli'
-const mongodbUrl = 'mongodb://127.0.0.1:27018/cjx-cli'
+const mongodbUrl = 'mongodb://root:cjx6Ufg8UFoQRtBMx@39.100.76.235:27017/cjx-cli'
+// const mongodbUrl = 'mongodb://127.0.0.1:27018/cjx-cli'
 
 // 数据库名称
 const mongodbDbName = 'cjx-cli'
