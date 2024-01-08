16dcd32127f9863504afdffcd154f482eba07d5a
shuihang <2680027613@qq.com>
2024-01-08 17:00:00 +0800

    fix: 修改Dockerfile

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const { Controller } = require('egg');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg test1';
+    ctx.body = 'hi, egg test2';
   }
 }
 
